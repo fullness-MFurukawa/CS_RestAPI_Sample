8d6b291384e93fa21fc370b656615f2856c81dda
Masatoshi Furukawa <furukawa@fullness.co.jp>
2025-07-03 18:23:23 +0900

    ドメインオブジェクトの追加

---
+++ Apps/RestAPI_Sample.Application/Domains/Models/Department.cs
@@ -0,0 +1,8 @@
+namespace RestAPI_Sample.Application.Domains.Models;
+/// <summary>
+/// 部署を表すドメインオブジェクト
+/// </summary>
+public class Department 
+{
+
+}
---
+++ Apps/RestAPI_Sample.Application/Domains/Models/Employee.cs
@@ -0,0 +1,8 @@
+namespace RestAPI_Sample.Application.Domains.Models;
+/// <summary>
+/// 従業員を表すドメインオブジェクト
+/// </summary>
+public class Employee Domain : IEquatable<Domain>
+{
+
+}
