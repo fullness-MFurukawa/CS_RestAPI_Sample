9c08312257c04a4240360e41e2f711558355369a
Masatoshi Furukawa <furukawa@fullness.co.jp>
2025-07-09 08:21:46 +0900

    従業員変更ユースケース変更

--- Apps/RestAPI_Sample.Application/Usecases/Employees/Interfaces/IUpdateEmployeeUseCase.cs
+++ Apps/RestAPI_Sample.Application/Usecases/Employees/Interfaces/IUpdateEmployeeUseCase.cs
@@ -1,8 +1,24 @@
+using RestAPI_Sample.Application.Domains.Models;
+
 namespace RestAPI_Sample.Application.Usecases.Employees.Interfaces;
 /// <summary>
 /// ユースケース:[従業員を変更する]を実現するインターフェイス
 /// </summary>
 public interface IUpdateEmployeeUsecase
 {
-    
+    /// <summary>
+    /// 指定された従業員Idの従業員を取得する
+    /// クライアント側の[削除画面]で利用するため
+    /// </summary>
+    /// <param name="id">従業員Id</param>
+    /// <returns>該当従業員</returns>
+    /// <exception cref="NotFoundException">従業員が存在しない場合にスローされる</exception>
+    Task<Employee> GetEmployeeByIdAsync(string id);
+
+    /// <summary>
+    /// 従業員を更新する
+    /// </summary>
+    /// <param name="employee">更新対象従業員</param>
+    /// <exception cref="NotFoundException">従業員が存在しない場合にスローされる</exception>
+    Task UpdateEmployee(Employee employee);
 }
